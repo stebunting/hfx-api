47a62ed7a0c3183fb28d8a3b9ceae1a7e3f67d84
Stephen Bunting <stebunting@gmail.com>
2021-05-02 22:38:48 +0200

    Add CORS Middleware

--- cmd/hfx-api/main.go
+++ cmd/hfx-api/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/stebunting/hfx-backend/cors"
 	"github.com/stebunting/hfx-backend/model"
 	"github.com/stebunting/hfx-backend/routes"
 )
@@ -20,11 +21,11 @@ func main() {
 
 	r := routes.ConfigRoutes(model.Db)
 
-	http.HandleFunc("/wake", r.Wake)
-	http.HandleFunc("/dbinit", r.DbInit)
-	http.HandleFunc("/updatecurrencies", r.UpdateCurrencies)
-	http.HandleFunc("/getcurrencies", r.GetCurrencies)
-	http.HandleFunc("/getrate", r.GetRate)
+	http.Handle("/wake", cors.Middleware(http.HandlerFunc(r.Wake)))
+	http.Handle("/dbinit", cors.Middleware(http.HandlerFunc(r.DbInit)))
+	http.Handle("/updatecurrencies", cors.Middleware(http.HandlerFunc(r.UpdateCurrencies)))
+	http.Handle("/getcurrencies", cors.Middleware(http.HandlerFunc(r.GetCurrencies)))
+	http.Handle("/getrate", cors.Middleware(http.HandlerFunc(r.GetRate)))
 
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
